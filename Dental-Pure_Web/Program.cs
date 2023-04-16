9e025f0929b0a4663614da805176c77c9e1db172
Kuba Wojciechowski <kuba99wojciechowski@gmail.com>
2023-04-16 22:55:07 +0200

    Navbar improvements

--- Dental-Pure_Web/Program.cs
+++ Dental-Pure_Web/Program.cs
@@ -1,8 +1,14 @@
+using Dental_Pure.Domain.Repository.IRepository;
+using Dental_Pure.Domain.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
