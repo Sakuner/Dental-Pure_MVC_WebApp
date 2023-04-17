ccbad96d366bfd94bafd19250908050a276da7dd
Kuba Wojciechowski <kuba99wojciechowski@gmail.com>
2023-04-17 15:47:07 +0200

    database creation, implementation of connection string

---
+++ Dental-Pure.DataAccess/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dental_Pure.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        //public DbSet<ReservationModel> Reservations { get; set; } 
+    }
+}
--- Dental-Pure_Web/Program.cs
+++ Dental-Pure_Web/Program.cs
@@ -1,11 +1,14 @@
 using Dental_Pure.Domain.Repository.IRepository;
 using Dental_Pure.Domain.Repository;
+using Dental_Pure.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
