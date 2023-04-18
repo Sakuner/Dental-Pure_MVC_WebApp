74889afdfc7c234fcb7b521f50a1825acd78cad6
Kuba Wojciechowski <kuba99wojciechowski@gmail.com>
2023-04-18 23:39:26 +0200

    added staff to database, started creating staff controller

---
+++ Dental-Pure_Web/Controllers/StaffController.cs
@@ -0,0 +1,18 @@
+﻿using Dental_Pure.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dental_Pure_Web.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly AppDbContext _db;
+        public StaffController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
